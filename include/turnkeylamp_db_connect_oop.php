2ac231b96956734a8e7b2570fd40c299b799c434
Low Kok Wei <98858885+lowkw@users.noreply.github.com>
2022-10-13 19:33:52 +0800

    Delete include directory

--- include/turnkeylamp_db_connect_oop.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "adminer", "P@ssw0rd", "taskTwoDB");
-if($mysqli === false) {
-	die("ERROR: Could not connect. " . $mysqli->connect_error);
-}
-
-$sql = "USE taskTwoDB";
-$mysqli->query($sql); 
-?>
--- include/xampp_db_connect_oop.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "", "taskTwoDB");
-if($mysqli === false) {
-	die("ERROR: Could not connect. " . $mysqli->connect_error);
-}
-
-$sql = "USE taskTwoDB";
-$mysqli->query($sql); 
-?>
